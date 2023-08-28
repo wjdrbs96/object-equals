50a61bb0c982a74ab3e1ddcf2ad369a67f0ee271
wjdrbd96 <wjdrbs966@naver.com>
2023-08-28 22:42:40 +0900

    init

---
+++ src/main/java/com/example/objectequals/ObjectEqualsApplication.java
@@ -0,0 +1,13 @@
+package com.example.objectequals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectEqualsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectEqualsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/objectequals/ObjectEqualsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.objectequals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectEqualsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
